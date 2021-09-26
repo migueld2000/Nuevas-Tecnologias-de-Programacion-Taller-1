13f0823768406511fdd61665882e6eee1b68d432
miguel david <migueld2000@gmail.com>
2021-09-26 08:29:38 -0500

    se crea el menu y se empiesa a codificarlo

--- src/jugador/Jugador.java
+++ src/jugador/Jugador.java
@@ -10,5 +10,85 @@ package jugador;
  * @author migue
  */
 public class Jugador {
+
+    private int ID;
+    private String Nombre;
+    private String Apalleido;
+    private String Edad;
+    private String Posicion;
+    private String Numero;
+    private String Nombre_del_Equio;
+
+    public Jugador() {
+    }
+
+    public Jugador(int ID, String Nombre, String Apalleido, String Edad, String Posicion, String Numero, String Nombre_del_Equio) {
+        this.ID = ID;
+        this.Nombre = Nombre;
+        this.Apalleido = Apalleido;
+        this.Edad = Edad;
+        this.Posicion = Posicion;
+        this.Numero = Numero;
+        this.Nombre_del_Equio = Nombre_del_Equio;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApalleido() {
+        return Apalleido;
+    }
+
+    public void setApalleido(String Apalleido) {
+        this.Apalleido = Apalleido;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getPosicion() {
+        return Posicion;
+    }
+
+    public void setPosicion(String Posicion) {
+        this.Posicion = Posicion;
+    }
+
+    public String getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(String Numero) {
+        this.Numero = Numero;
+    }
+
+    public String getNombre_del_Equio() {
+        return Nombre_del_Equio;
+    }
+
+    public void setNombre_del_Equio(String Nombre_del_Equio) {
+        this.Nombre_del_Equio = Nombre_del_Equio;
+    }
+
     
-}
+    
+    
+}  
--- src/seleccion_colombia/Seleccion_colombia.java
+++ src/seleccion_colombia/Seleccion_colombia.java
@@ -5,6 +5,9 @@
  */
 package seleccion_colombia;
 
+import java.util.Scanner;
+import jugador.Jugador;
+
 /**
  *
  * @author migue
@@ -15,7 +18,63 @@ public class Seleccion_colombia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+        int opcion;
+        int contador = 0;
+        
+        Jugador [] jugadores=new Jugador[3];
+        
+        do
+        {
+            System.out.println("Bienbenido");
+            System.out.println("****************************");
+            System.out.println("1. Agregar un jagador");
+            System.out.println("2. Buscar un jugador por el numero");
+            System.out.println("3. Editar a un jugador");
+            System.out.println("4. Mostrar toda la convocatoria de jugadores");
+            System.out.println("5. Salir");
+            
+            System.out.println("Digite la opcion: ");
+            opcion=entrada.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    jugadores[contador]=new Jugador();
+                    System.out.println("Digite el id del jugador");
+                    jugadores[contador].setID(entrada.nextInt());
+                    System.out.println("Digite el Nombre del jugador");
+                    jugadores[contador].setNombre(entrada.next());
+                    System.out.println("Digite el Apellido del jugador");
+                    jugadores[contador].setApalleido(entrada.next());
+                    System.out.println("Digite la edad del jugador");
+                    jugadores[contador].setEdad(entrada.next());
+                    System.out.println("Digite la posicion del jugador");
+                    jugadores[contador].setPosicion(entrada.next());
+                    System.out.println("Digite el numero del jugador");
+                    jugadores[contador].setNumero(entrada.next());
+                    System.out.println("Digite el nombre del equipo del jugador");
+                    jugadores[contador].setNombre_del_Equio(entrada.next());
+                    break;
+                case 2:
+                    System.out.println("Digite la posicion del jugador");
+                    
+                    
+                    
+                    break;
+                    
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }while(opcion!=5);
+        
     }
     
 }
