e0ead9616e745f1d152901f66e66e07eb9f023f9
miguel david <migueld2000@gmail.com>
2021-10-01 18:08:56 -0500

    se continua configurando el menu

--- src/seleccion_colombia/Seleccion_colombia.java
+++ src/seleccion_colombia/Seleccion_colombia.java
@@ -60,20 +60,84 @@ public class Seleccion_colombia {
                         System.out.println("Ya no se puede metrar mas jugadores");
                     }
                     break;
+                    
                 case 2:
-                    System.out.println("Digite la posicion del jugador");
+                    System.out.println("Digite el numero del jugador");
+                    jugadores[contador].setNumero(entrada.next());
+                
                     
-                    
-                    
+                    String setNumero = null;
+                    for(int i=0;i<contador;i++)
+                    {
+                        
+                        if(setNumero==jugadores[i].getNumero())
+                        {
+                            System.out.println(jugadores[i].getID());
+                            System.out.println(jugadores[i].getNombre());
+                            System.out.println(jugadores[i].getApalleido());
+                            System.out.println(jugadores[i].getEdad());
+                            System.out.println(jugadores[i].getNombre_del_Equio());
+                            System.out.println(jugadores[i].getPosicion());
+                            
+                        }
+                        else
+                        {
+                            System.out.println("El jugaor buscado no se encuentra registrado");
+                        }
+                        
+                    }                  
                     break;
                     
                 case 3:
+                    
+                    System.out.println("Digite el id del jugador");
+                    jugadores[contador].setID(entrada.nextInt());
+                    
+                    int setID = 0;
+
+                    for(int i=0;i<contador;i++)
+                    {
+
+                        if(setID==jugadores[i].getID())
+                        {
+                            System.out.println("Id actual del jugador");
+                            System.out.println(jugadores[i].getID());
+                            
+                            
+                            
+                            System.out.println("Nombre actual del jugador");
+                            System.out.println(jugadores[i].getNombre());
+                            
+                            System.out.println("Apellido actual del jugador");
+                            System.out.println(jugadores[i].getApalleido());
+                            
+                            System.out.println("Edad actual del jugador");
+                            System.out.println(jugadores[i].getEdad());
+                            
+                            System.out.println("Nombre del equipo actual del jugador");
+                            System.out.println(jugadores[i].getNombre_del_Equio());
+                            
+                            System.out.println("Posicion actual del jugador");
+                            System.out.println(jugadores[i].getPosicion());
+                            
+                        }
+                        
+                        else
+                        {
+                            System.out.println("El jugaor buscado no se encuentra registrado");
+                        }
+                        
+                    }            
+
                     break;
+                    
                 case 4:
                     break;
+                    
                 case 5:
                     System.exit(0);
                     break;
+                    
                 default:
                     System.out.println("Opcion no valida");
                     break;
